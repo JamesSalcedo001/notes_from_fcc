57d7ca2a1c2230eb9483a609dfafbeb8079b7563
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-11-02 20:54:25 -0500

    Demonstrate loops structure, iterator section, i = 0 gives the i variable value of 0 first time loop runs

--- js_algo_ds/pyramid_generator/index.js
+++ js_algo_ds/pyramid_generator/index.js
@@ -1,11 +1,11 @@
-let character = "Hello";
-let count = 8;
-let rows = ["Naomi", "Quincy", "CamperChan"];
-console.log(rows[0]);
+// let character = "Hello";
+// let count = 8;
+// let rows = ["Naomi", "Quincy", "CamperChan"];
+// console.log(rows[0]);
 
 // arrays are mutable, you can change the value at index directly
 
-rows[2] = 10;
+// rows[2] = 10;
 // console.log(rows);
 
 // mutation = change
@@ -14,11 +14,11 @@ rows[2] = 10;
 
 // rows[rows.length - 1] = 10;
 
-let cities = ["London", "New York", "Mumbai"];
-cities[cities.length - 1] = "Mexico City";
+// let cities = ["London", "New York", "Mumbai"];
+// cities[cities.length - 1] = "Mexico City";
 
 
-console.log(cities);
+// console.log(cities);
 
 // method in js is a function that's associated with certain values or objects
 
@@ -26,14 +26,36 @@ console.log(cities);
 
 // arrays have methods. push method allows pushing a value to the end of the array. 
 
-rows.push("freeCodeCamp");
+// rows.push("freeCodeCamp");
 
 // pop method removes last element from array and returns the element
 
-let popped = rows.pop();
-console.log(popped);
+// let popped = rows.pop();
+// console.log(popped);
 
-let pushed = rows.push("freeCodeCamp");
-console.log(pushed);
+// let pushed = rows.push("freeCodeCamp");
+// console.log(pushed);
 
 //  push returns the new length of the array, after adding the new value, not the actual value added
+
+// declaring with let keyword allows it to be reassigned so you can change a value later on. 
+
+// const variables cannot be reassigned like let variable. it also cannot be uninitialized whereas let can. 
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+// when you have to perform a task repeatedly until a condition is met, you use a loop. for loop:
+
+// for (iterator; condition; iteration) {
+// logic;
+// }
+
+// the iterator is a variable you can declare specifically in for loop to control how the loop iterates or goes through your logic
+
+// Assign it the value 0 to start. This will give the i variable the value 0 the first time your loop runs.
+
+for (let i = 0; ) {
+    
+}

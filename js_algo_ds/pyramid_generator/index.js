9a5ceb06192cda2cc446519e43a76e4debdf69f2
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-11-02 20:29:04 -0500

    Notes on arrays using length method

--- js_algo_ds/pyramid_generator/index.js
+++ js_algo_ds/pyramid_generator/index.js
@@ -6,5 +6,14 @@ console.log(rows[0]);
 // arrays are mutable, you can change the value at index directly
 
 rows[2] = 10;
-console.log(rows);
+// console.log(rows);
 
+// mutation = change
+
+// array.length returns number of elems in array. by subtracting 1, you get index of last element in array. 
+
+// rows[rows.length - 1] = 10;
+
+let cities = ["London", "New York", "Mexico City"];
+
+console.log(cities);

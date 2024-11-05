8fa7b5ae29332c7c3f8cc09d7febe5655eae9b1f
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-11-05 16:12:13 -0600

    Current implementation is a clean and functional version

--- js_algo_ds/pyramid_generator/index.js
+++ js_algo_ds/pyramid_generator/index.js
@@ -198,11 +198,9 @@ function padRow(rowNumber, rowCount) {
 // }
 
 
-let done = 0;
 
-while (done <= count) {
-    done++;
-    rows.push(padRow(done, count));
+while (rows.length < count) {
+    rows.push(padRow(rows.length + 1, count));
 }
 
   

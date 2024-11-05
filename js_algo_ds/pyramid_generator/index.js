79691cba6b890d8aca7a6c8eaa4cc025f36c13fb
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-11-05 17:41:31 -0600

    Add else clause to if statement

--- js_algo_ds/pyramid_generator/index.js
+++ js_algo_ds/pyramid_generator/index.js
@@ -239,7 +239,9 @@ function padRow(rowNumber, rowCount) {
 for (let i = 1; i <= count; i++) {
     if (inverted) {
         rows.unshift(padRow(i, count));
-    } 
+    } else {
+        rows.push(padRow(i, count));
+    }
 }
 
 
